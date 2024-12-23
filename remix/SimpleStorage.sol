b001fd40d2ccf79368b9cab13df9fcdfa67a68a7
Toby <0xdegn@gmail.com>
2024-12-23 12:07:37 +0100

    Modified src/SimpleStorage.sol

--- remix/SimpleStorage.sol
+++ remix/SimpleStorage.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.19;     //  Solidity version, 0.8.18 and above
+pragma solidity ^0.8.19; //  Solidity version, 0.8.18 and above
 
 contract SimpleStorage {
     // bool hasFavouriteNumber = true;
@@ -9,22 +9,19 @@ contract SimpleStorage {
     // address myAdress = 0x6f451aB7946458550D97a318927381C51870F587
     // bytes32 favouriteBytes32 = "cat"
 
-
     // favouriteNumber gets in itialized to 0 if no value is specified.
 
-    // uint256 public myFavouriteNumber; 
+    // uint256 public myFavouriteNumber;
 
     uint256 myFavouriteNumber = 0;
 
     function store(uint256 _favouriteNumber) public virtual {
-        myFavouriteNumber = _favouriteNumber; 
+        myFavouriteNumber = _favouriteNumber;
     }
 
-    function retrieve() public view returns(uint256) {
+    function retrieve() public view returns (uint256) {
         return myFavouriteNumber;
     }
-
-
 
     // uint256[] listOfFavouriteNumbers;  // Stores an array of uint256 datatypes
 
@@ -33,14 +30,14 @@ contract SimpleStorage {
         uint256 favouriteNumber;
         string name;
     }
- 
+
     // Person[3] public listOfPeople;  // Static array - Max length is 3.
-    Person[] public listOfPeople;   // Dynamic array - Can grow to any length.
+    Person[] public listOfPeople; // Dynamic array - Can grow to any length.
 
     mapping(string => uint256) public nameToFavoriteNumber;
 
     // Person public patrick = Person(7, "Patrick"); // The values are assigned respectively, favouriteNumber takes 7, while name takes "pat".
-    // Person public patrick = Person({name: "Patrick", favouriteNumber: 7});
+    // Person public patrick = Person({name: "Patrick", favouriteNumber: 7});  // Assigned explicitly.
     // Person public mariah = Person({name: "Mariah", favouriteNumber: 23});
     // Person public john = Person({name: "John", favouriteNumber: 16});
 
