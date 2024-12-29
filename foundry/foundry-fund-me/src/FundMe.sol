9c4de2e425f2f78b63c59bb588d0b81a247c8c6a
Toby <0xdegn@gmail.com>
2024-12-29 19:01:11 +0100

    Refactored and wrote some tests

--- foundry/foundry-fund-me/src/FundMe.sol
+++ foundry/foundry-fund-me/src/FundMe.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-// Note: The AggregatorV3Interface might be at a different location than what was in the video!
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 
@@ -10,8 +9,8 @@ error FundMe__NotOwner();
 contract FundMe {
     using PriceConverter for uint256;
 
-    mapping(address => uint256) public addressToAmountFunded;
-    address[] public funders;
+    mapping(address => uint256) private s_addressToAmountFunded; // s_ means it's a storage variable
+    address[] private s_funders;
 
     address public immutable i_owner;
     uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
@@ -23,13 +22,9 @@ contract FundMe {
     }
 
     function fund() public payable {
-        require(
-            msg.value.getConversionRate() >= MINIMUM_USD,
-            "You need to spend more ETH!"
-        );
-        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
-        addressToAmountFunded[msg.sender] += msg.value;
-        funders.push(msg.sender);
+        require(msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD, "You need to spend more ETH!");
+        s_addressToAmountFunded[msg.sender] += msg.value;
+        s_funders.push(msg.sender);
     }
 
     function getVersion() public view returns (uint256) {
@@ -46,39 +41,16 @@ contract FundMe {
     }
 
     function withdraw() public onlyOwner {
-        for (
-            uint256 funderIndex = 0;
-            funderIndex < funders.length;
-            funderIndex++
-        ) {
-            address funder = funders[funderIndex];
-            addressToAmountFunded[funder] = 0;
+        for (uint256 funderIndex = 0; funderIndex < s_funders.length; funderIndex++) {
+            address funder = s_funders[funderIndex];
+            s_addressToAmountFunded[funder] = 0;
         }
-        funders = new address[](0);
-        // // transfer
-        // payable(msg.sender).transfer(address(this).balance);
 
-        // // send
-        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
-        // require(sendSuccess, "Send failed");
+        s_funders = new address[](0);
 
-        // call
-        (bool callSuccess, ) = payable(msg.sender).call{
-            value: address(this).balance
-        }("");
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
         require(callSuccess, "Call failed");
     }
-    // Explainer from: https://solidity-by-example.org/fallback/
-    // Ether is sent to contract
-    //      is msg.data empty?
-    //          /   \
-    //         yes  no
-    //         /     \
-    //    receive()?  fallback()
-    //     /   \
-    //   yes   no
-    //  /        \
-    //receive()  fallback()
 
     fallback() external payable {
         fund();
@@ -87,13 +59,14 @@ contract FundMe {
     receive() external payable {
         fund();
     }
-}
 
-// Concepts we didn't cover yet (will cover in later sections)
-// 1. Enum
-// 2. Events
-// 3. Try / Catch
-// 4. Function Selector
-// 5. abi.encode / decode
-// 6. Hash with keccak256
-// 7. Yul / Assembly
+    // VIEW / PURE FUNCTIONS UNDERNEATH (Getters)
+
+    function getaddressToAmountFunded(address fundingAddress) external view returns (uint256) {
+        return (s_addressToAmountFunded[fundingAddress]);
+    }
+
+    function getFunders(uint256 index) external view returns (address) {
+        return (s_funders[index]);
+    }
+}
--- foundry/foundry-fund-me/test/FundMeTest.t.sol
+++ foundry/foundry-fund-me/test/FundMeTest.t.sol
@@ -4,12 +4,21 @@ pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
 
+    uint256 constant SEND_VALUE = 0.1 ether;    // 100000000000000000
+    uint256 constant STARTING_BALANCE = 10 ether;    // 100000000000000000
+    address USER = makeAddr("user");
+
+    // This is run before any other function in the contract.
     function setUp() external {
-        fundMe = new FundMe();
+        // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE);
     }
 
     function testMinDollarIsFive() public view {
@@ -20,12 +29,26 @@ contract FundMeTest is Test {
     function testOwnerIsMsgSender() public view {
         console.log(fundMe.i_owner()); // This is the address of the contract deployer for FundMe i.e FundMeTest()
         console.log(msg.sender); // This is our address
-        assertEq(fundMe.i_owner(), address(this));
+        console.log(address(this)); // This is the address of this contract.
+        assertEq(fundMe.i_owner(), msg.sender);
     }
 
     function testPriceFeedVersionIsAccurate() public view {
         uint256 version = fundMe.getVersion();
         console.log(version);
         assertEq(version, 4);
+    }
+
+    function testFundFailsWithoutEnoughEth() public {
+        vm.expectRevert();  // Next line should revert only if it fails.
+        fundMe.fund();      // Send 0 Eth (OFC this would fail because its not up to $5)
+    }
+
+    function testFundUpdatesFUndedDataStructure() public {
+        vm.prank(USER);     // The next tx will be sent by this address
+        fundMe.fund{value: SEND_VALUE}();
+
+        uint256 amountFUnded = fundMe.getaddressToAmountFunded(USER);
+        assertEq(amountFUnded, SEND_VALUE);
     }
 }
